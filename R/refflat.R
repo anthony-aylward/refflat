7ca5890338b9e4712fb4cfac5b9027e7b907504e
Anthony Aylward <aaylward@eng.ucsd.edu>
2018-10-29 03:25:54 -0700

    test

--- R/refflat.R
+++ R/refflat.R
@@ -45,6 +45,14 @@ fetch_refflat_data <- function(tmpdir = tempdir(), method = "auto") {
   refflat_data
 }
 
+directional_gene_coordinates <- function(start, end, strand = c("+", "-")) {
+  if (strand == "+") {
+    c(start, end)
+  } else if (strand == "-") {
+    c(end, start)
+  }
+}
+
 #' Plot a gene
 #'
 #' Plot a gene
@@ -52,7 +60,25 @@ fetch_refflat_data <- function(tmpdir = tempdir(), method = "auto") {
 #' @param name character. The name of the gene to plot.
 #' @param refflat data frame. The refflat dataset.
 #' @export
-plot_gene <- function(name, refflat = refflat_data) {
-  gene <- refflat[refflat]
-  lines()
+plot_gene <- function(
+  name,
+  refflat = refflat_data,
+  y_coord = 0,
+  flatten = FALSE
+) {
+  gene_data <- refflat[,
+    refflat[["geneName"]] == name | refflat[["name"]] == name
+  ]
+  gene_length <- gene_data[["cdsEnd"]] - gene_data[["cdsStart"]]
+  gene_center <- (gene_data[["cdsStart"]] + gene_data[["cdsEnd"]]) / 2
+  plot(
+    rep(c(gene_center - gene_length, gene_center + gene_length), 2),
+    rep(c(y_coord, y_coord + 1, 2),
+    col="white"
+  )
+  if (strand == "+") {
+    arrows(gene_data[1, "cdsStart"], 0.5, x1 = gene_data[1, "cdsEnd"], lwd = 4)
+  } else if (strand == "-") {
+    arrows(gene_data[1, "cdsEnd"], 0.5, gene_data[1, "cdsStart"], lwd = 4)
+  }
 }
