72fa31bb8703a57995215c551b942d023de60ab0
Anthony Aylward <aaylward@eng.ucsd.edu>
2018-10-29 05:12:19 -0700

    draw gene

--- R/refflat.R
+++ R/refflat.R
@@ -45,6 +45,102 @@ fetch_refflat_data <- function(tmpdir = tempdir(), method = "auto") {
   refflat_data
 }
 
+#' Slice the refFlat dataset
+#'
+#' Slice the refFlat dataset
+#'
+#' @param chrom character. The chromosome of the slice.
+#' @param start integer. The start position of the slice.
+#' @param end integer. The end position of the slice.
+#' @param refflat data frame. The refflat dataset.
+#' @return A data frame representing the requested slice of the refflat data.
+#' @export
+slice_refflat <- function(chrom, start, end, refflat = refflat_data) {
+  refflat[
+    (
+      refflat[["chrom"]] == chrom 
+      & refflat[["cdsStart"]] < end 
+      & refflat[["cdsEnd"]] > start
+    ),
+  ]
+}
+
+#' Flatten refFlat data
+#'
+#' Flatten refFlat data
+#'
+#' @param chrom character. The chromosome of the slice.
+#' @param start integer. The start position of the slice.
+#' @param end integer. The end position of the slice.
+#' @param refflat data frame. The refflat dataset.
+#' @return A data frame representing the requested slice of the refflat data.
+#' @export
+flatten_refflat <- function(refflat) {
+  as.data.frame(
+    do.call(
+      "rbind",
+      lapply(
+        unique(refflat[["geneName"]]),
+        function(gene_name) {
+          gene_data <- refflat[refflat[["geneName"]] == gene_name,]
+          list(
+            geneName = gene_data[1, "geneName"],
+            chrom = gene_data[1, "chrom"],
+            strand = gene_data[1, "strand"],
+            cdsStart = min(gene_data[["cdsStart"]]),
+            cdsEnd = max(gene_data[["cdsEnd"]])
+          )
+        }
+      )
+    )
+  )
+}
+
+
+#' Draw a gene on a plot
+#'
+#' Draw a gene on a plot
+#'
+#' @param gene_data list. List representing the gene.
+#' @param y numeric. The y coordinate at which to draw the gene.
+#' @param arrowhead_length numeric. Length of the edges of the arrow head (in
+#'   inches).
+#' @param angle numeric. Angle from the shaft of the arrow to the edge of the
+#'   arrow head.
+#' @param lwd numeric. Weight of lines.
+#' @export
+draw_gene <- function(
+  gene_data,
+  y,
+  arrowhead_length = 0.125,
+  angle = 15,
+  lwd = 2
+) {
+  gene_length <- gene_data[["cdsEnd"]] - gene_data[["cdsStart"]]
+  gene_center <- (gene_data[["cdsStart"]] + gene_data[["cdsEnd"]]) / 2
+  if (gene_data[["strand"]] == "+") {
+    arrowhead_code = 2
+  } else if (gene_data[["strand"]] == "-") {
+    arrowhead_code = 1
+  }
+  arrows(
+    gene_data[["cdsStart"]],
+    y_coord + 0.5,
+    x1 = gene_data[["cdsEnd"]],
+    length = arrowhead_length,
+    angle = angle,
+    code = arrowhead_code,
+    lwd = lwd
+  )
+  text(
+    gene_center,
+    y_coord + 0.5,
+    labels = gene_data[["geneName"]],
+    font = 3,
+    pos = 3
+  )
+}
+
 #' Plot a gene
 #'
 #' Plot a gene
@@ -102,55 +198,4 @@ plot_gene <- function(
     lwd = lwd
   )
   text(gene_center[[1]], y_coord + 0.5, labels = name, font = 3, pos = 3)
-}
-
-#' Slice the refFlat dataset
-#'
-#' Slice the refFlat dataset
-#'
-#' @param chrom character. The chromosome of the slice.
-#' @param start integer. The start position of the slice.
-#' @param end integer. The end position of the slice.
-#' @param refflat data frame. The refflat dataset.
-#' @return A data frame representing the requested slice of the refflat data.
-#' @export
-slice_refflat <- function(chrom, start, end, refflat = refflat_data) {
-  refflat[
-    (
-      refflat[["chrom"]] == chrom 
-      & refflat[["cdsStart"]] < end 
-      & refflat[["cdsEnd"]] > start
-    ),
-  ]
-}
-
-#' Flatten refFlat data
-#'
-#' Flatten refFlat data
-#'
-#' @param chrom character. The chromosome of the slice.
-#' @param start integer. The start position of the slice.
-#' @param end integer. The end position of the slice.
-#' @param refflat data frame. The refflat dataset.
-#' @return A data frame representing the requested slice of the refflat data.
-#' @export
-flatten_refflat <- function(refflat) {
-  as.data.frame(
-    do.call(
-      "rbind",
-      lapply(
-        unique(refflat[["geneName"]]),
-        function(gene_name) {
-          gene_data <- refflat[refflat[["geneName"]] == gene_name,]
-          list(
-            geneName = gene_data[1, "geneName"],
-            chrom = gene_data[1, "chrom"],
-            strand = gene_data[1, "strand"],
-            cdsStart = min(gene_data[["cdsStart"]]),
-            cdsEnd = max(gene_data[["cdsEnd"]])
-          )
-        }
-      )
-    )
-  )
 }
