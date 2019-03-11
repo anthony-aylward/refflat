00d9a734c9dd4421b1e06870562132debf8440d2
Anthony Aylward <aaylward@eng.ucsd.edu>
2019-03-11 11:32:36 -0700

    finish up

---
+++ R/t2d_fto.R
@@ -0,0 +1,13 @@
+#' Example GWAS data
+#'
+#' Featuring a T2D signal at the FTO locus
+#'
+#' @format A data frame with 3246 rows (SNPs) and 4 variables:
+#' \describe{
+#'   \item{MARKER}{string describing a SNP}
+#'   \item{CHR}{chromosome of the SNP coordinate}
+#'   \item{POS}{position of the SNP coordinate}
+#'   \item{PVALUE}{P-value of the T2D association}
+#' }
+#' @source \url{https://www.nature.com/articles/s41588-018-0332-4}
+"t2d_fto"
