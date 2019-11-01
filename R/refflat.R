8db07db7456328763d86d2cf051fb5fda2ca0d9f
Anthony Aylward <aaylward@eng.ucsd.edu>
2019-10-31 23:54:08 -0700

    try this

--- R/refflat.R
+++ R/refflat.R
@@ -202,12 +202,12 @@ determine_levels <- function(rf, buffer = 0) {
   for (row in 2:nrow(rf)) {
     broke_loop <- FALSE
     for (l in 1:max(levels)) {
-      print(ends[level])
-      print(rf[row, "txStart"])
-      if (ends[level] + buffer < rf[row, "txStart"]) {
-        level <- l
-        broke_loop <- TRUE
-        break
+      if (!is.na(rf[row, "txStart"])) {
+        if (ends[level] + buffer < rf[row, "txStart"]) {
+          level <- l
+          broke_loop <- TRUE
+          break
+        }
       }
     }
     if (!broke_loop) {
